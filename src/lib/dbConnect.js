21fcae38dd1c681eb7c5b03adccabca8db7d0139
md-zeon <zeonrahaman5870@gmail.com>
2025-08-03 15:28:29 +0600

    Connected to mongodb

---
+++ src/lib/dbConnect.js
@@ -0,0 +1,15 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+const uri = process.env.MONGODB_URI;
+
+export default function dbConnect(collectionName) {
+	// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+	const client = new MongoClient(uri, {
+		serverApi: {
+			version: ServerApiVersion.v1,
+			strict: true,
+			deprecationErrors: true,
+		},
+	});
+
+    return client.db(process.env.DB_NAME).collection(collectionName);
+}
